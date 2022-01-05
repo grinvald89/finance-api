c41c9445bd5dffed7dd4ce0f3437952ab55f4b05
grinvald89 <grinvald89@ya.ru>
2022-01-05 07:32:34 +0300

    Добавлен контроллер для чтения и записи пользовательских ролей

---
+++ finance-api/Controllers/UserRolesController.cs
@@ -0,0 +1,54 @@
+﻿using finance_api.Data;
+using finance_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace finance_api.Controllers
+{
+    [Route("api/user-roles")]
+    [ApiController]
+    public class UserRolesController : ControllerBase
+    {
+        private readonly FinanceDbContext _dbContext;
+
+        public UserRolesController(FinanceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public List<UserRole> Get()
+        {
+            return _dbContext.UserRoles.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public UserRole? Get(Guid id)
+        {
+            List<UserRole> roles = _dbContext.UserRoles.ToList();
+
+            return roles.Find(t => t.Id == id);
+        }
+
+        [HttpPost]
+        public IActionResult Post(UserRoleCreateRequest request)
+        {
+            UserRole role = new UserRole()
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name
+            };
+
+            _dbContext.UserRoles.Add(role);
+            _dbContext.SaveChanges();
+
+            return Ok(role);
+        }
+
+        public class UserRoleCreateRequest
+        {
+            public string Name { get; set; }
+        }
+    }
+}

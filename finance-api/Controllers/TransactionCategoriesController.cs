06b4358847220bed76e99d8a1aad8c1d9e7a191c
grinvald89 <grinvald89@ya.ru>
2022-01-05 07:31:42 +0300

    Добавлен контроллер для чтения и записи категорий транзакций

---
+++ finance-api/Controllers/TransactionCategoriesController.cs
@@ -0,0 +1,54 @@
+﻿using finance_api.Data;
+using finance_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace finance_api.Controllers
+{
+    [Route("api/transaction-categories")]
+    [ApiController]
+    public class TransactionCategoriesController : ControllerBase
+    {
+        private readonly FinanceDbContext _dbContext;
+
+        public TransactionCategoriesController(FinanceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public List<TransactionCategory> Get()
+        {
+            return _dbContext.TransactionCategories.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public TransactionCategory? Get(Guid id)
+        {
+            List<TransactionCategory> categories = _dbContext.TransactionCategories.ToList();
+
+            return categories.Find(t => t.Id == id);
+        }
+
+        [HttpPost]
+        public IActionResult Post(TransactionCategoryCreateRequest request)
+        {
+            TransactionCategory category = new TransactionCategory()
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name
+            };
+
+            _dbContext.TransactionCategories.Add(category);
+            _dbContext.SaveChanges();
+
+            return Ok(category);
+        }
+
+        public class TransactionCategoryCreateRequest
+        {
+            public string Name { get; set; }
+        }
+    }
+}

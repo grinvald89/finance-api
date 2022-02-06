8f6b3899a741152bec011ad1802ff8ba3a1cb1a0
grinvald89 <grinvald89@ya.ru>
2022-02-06 20:16:45 +0300

    Добавлены CRUD операции для статусов транзакций

--- finance-api/Controllers/TransactionStatusesController.cs
+++ finance-api/Controllers/TransactionStatusesController.cs
@@ -20,7 +20,10 @@ namespace finance_api.Controllers
         [HttpGet]
         public List<TransactionStatus> Get()
         {
-            List<TransactionStatus> statuses = _dbContext.TransactionStatuses.ToList().FindAll(s => !s.Deleted);
+            List<TransactionStatus> statuses =
+                _dbContext.TransactionStatuses
+                    .ToList()
+                    .FindAll(s => !s.Deleted);
 
             return statuses;
         }
@@ -28,15 +31,23 @@ namespace finance_api.Controllers
         [HttpGet("{id}")]
         public TransactionStatus? Get(Guid id)
         {
-            List<TransactionStatus> statuses = _dbContext.TransactionStatuses.ToList().FindAll(s => !s.Deleted);
+            // Todo: проверка на id и существование статуса
+
+            List<TransactionStatus> statuses =
+                _dbContext.TransactionStatuses
+                    .ToList()
+                    .FindAll(s => !s.Deleted);
 
             return statuses.Find(t => t.Id == id);
         }
 
-        [HttpPost]
-        public IActionResult Post(TransactionStatusCreateRequest request)
+        [HttpPut]
+        public IActionResult Put(TransactionStatusRequest request)
         {
-            bool isStatusExist = _dbContext.TransactionStatuses.ToList()
+            bool isStatusExist =
+                _dbContext.TransactionStatuses
+                .ToList()
+                .FindAll(s => !s.Deleted)
                 .Find(s => s.Name == request.Name) != null;
 
             if (isStatusExist)
@@ -57,9 +68,50 @@ namespace finance_api.Controllers
             return Ok(status);
         }
 
-        public class TransactionStatusCreateRequest
+        [HttpPost]
+        public IActionResult Post(TransactionStatusRequest request)
         {
+            // Todo: проверка на id и существование статуса
+
+            TransactionStatus status =
+                _dbContext.TransactionStatuses
+                    .ToList()
+                    .FindAll(s => !s.Deleted)
+                    .Find(s => Guid.Equals(s.Id, request.Id));
+
+            status.Name = request.Name;
+
+            _dbContext.SaveChanges();
+
+            return Ok(status);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(TransactionStatusIdRequest request)
+        {
+            // Todo: проверка на id и существование статуса
+
+            TransactionStatus status =
+                _dbContext.TransactionStatuses
+                    .ToList()
+                    .Find(s => Guid.Equals(s.Id, request.Id));
+
+            status.Deleted = true;
+
+            _dbContext.SaveChanges();
+
+            return Ok(status);
+        }
+
+        public class TransactionStatusRequest
+        {
+            public Guid Id{ get; set; }
             public string Name { get; set; }
+        }
+
+        public class TransactionStatusIdRequest
+        {
+            public Guid Id { get; set; }
         }
     }
 }
